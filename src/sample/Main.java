48433caa53fd6aa35ddecf41f90b01d0282954d4
Marius <jaunikis@gmail.com>
2018-02-22 06:33:13 +0000

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,43 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        GridPane root= new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+
+        primaryStage.setTitle("Hi Java FX");
+        primaryStage.setScene(new Scene(root, 400, 300));
+
+        Label greeting = new Label("welcome to java fx");
+        greeting.setTextFill(Color.GREEN);
+        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD,40));
+        root.getChildren().add(greeting);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+}
