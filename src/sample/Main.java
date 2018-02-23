2106ab2dc683b7fe55ead2958cbd244d07bdf46a
Marius <jaunikis@gmail.com>
2018-02-23 06:19:22 +0000

    txt

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,20 +18,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        GridPane root= new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setVgap(10);
-        root.setHgap(10);
+//        GridPane root= new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
 
         primaryStage.setTitle("Hi Java FX");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setScene(new Scene(root, 500, 400));
 
-        Label greeting = new Label("welcome to java fx");
-        greeting.setTextFill(Color.GREEN);
-        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD,40));
-        root.getChildren().add(greeting);
+//        Label greeting = new Label("welcome to java fx");
+//        greeting.setTextFill(Color.DEEPSKYBLUE);
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD,30));
+//        root.getChildren().add(greeting);
+
         primaryStage.show();
     }
 
